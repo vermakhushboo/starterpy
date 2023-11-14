d066a314dd391b15964d7f6bf5df1e0a3f54754e
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2023-11-14 19:14:14 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ def main(context):
     if context.req.method == "GET":
         # Send a response with the res object helpers
         # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World from Khushboo!")
+        return context.res.send("Hello, World from main!")
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
