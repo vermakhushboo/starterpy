c12a586fae4769455b9e169db191ca86b3b7ee02
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-09-20 22:17:55 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -27,11 +27,6 @@ def main(context):
         return context.res.send("Hello, World from Kh!")
 
     # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
+    return context.res.binary(
+        bytearray([72, 105]))
     )
