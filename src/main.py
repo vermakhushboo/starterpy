a3245fec8dfa4b6aadadba46f82004ffc7000ee4
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-09-25 18:02:24 +0530

    Changes for testing to_string

--- src/main.py
+++ src/main.py
@@ -5,28 +5,4 @@ import os
 # This is your Appwrite function
 # It's executed each time we get a request
 def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
-
-    # You can log messages to the console
-    context.log("Hello, Logs!")
-
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
-
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World from Kh!")
-
-    # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.binary(
-        bytearray([72, 105])
-    )
+    return context.res.text(context.req.body_text)
