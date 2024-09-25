31265a68b2c2195875f2a0cd2a65297501069a9e
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2024-09-25 18:10:29 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ import os
 # This is your Appwrite function
 # It's executed each time we get a request
 def main(context):
-    print(context.req.body_text)
+    context.log(context.req.body_text)
     return context.res.text(context.req.body_text)
